85d466a0ffe3a8553c6841d740365e2b7f5e1699
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2018-10-16 15:42:09 -0300

    Passo 2.2 - Configurando o módulo principal

--- exemplo-talk/src/app/app.module.ts
+++ exemplo-talk/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ThfModule } from '@totvs/thf-ui';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ThfModule
   ],
   providers: [],
   bootstrap: [AppComponent]
